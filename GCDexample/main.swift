c37d43a8c3b210696b8eda21e61ef1839ad8f68a
Mirjigit <alaimirjigit154@gmail.com>
2023-09-20 22:16:50 +0600

    Initial commit

--- GCDexample/main.swift
+++ GCDexample/main.swift
@@ -1,5 +1,3 @@
-//
-//  main.swift
 //  GCDexample
 //
 //  Created by Миржигит Суранбаев on 20/9/23.
@@ -7,5 +5,55 @@
 
 import Foundation
 
-print("Hello, World!")
+print("Start")
 
+let queueMain = DispatchQueue.main
+
+let userQueue = DispatchQueue.global()
+
+let queueGlobal1 = DispatchQueue.global()
+let queueGlobal2 = DispatchQueue.global()
+
+DispatchQueue.main.async {
+    for i in 0 ... 10{
+        print(i)
+    }
+}
+sleep(1)
+
+
+queueGlobal1.async {
+    taskes("🤑")
+    
+    
+    sleep(1)
+    
+}
+queueGlobal2.sync {
+    taskes("👹")
+    
+    
+    sleep(1)
+    
+}
+
+
+userQueue.sync {
+    taskes("🤡")
+    
+    
+    sleep(1)
+    
+}
+
+sleep(1)
+
+
+dispatchMain()
+
+
+func taskes(_ simvol: String){
+    for i in 0...10 {
+        print("\(simvol) \(i) приоритет = \(qos_class_self().rawValue)")
+    }
+}
